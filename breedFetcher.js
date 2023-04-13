42bda162792f86baab0c8350e396773b2deb7ff3
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-13 19:28:23 -0400

    initial commit / fetching siberian data

---
+++ breedFetcher.js
@@ -0,0 +1,13 @@
+const request = require('request');
+
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+  console.log('error:', error); 
+  console.log('statusCode:', response && response.statusCode); 
+  // console.log('body:', body); 
+  // console.log(typeof body)
+  const data = JSON.parse(body);
+  // console.log(data);
+  // console.log(typeof data);
+
+  console.log(data[0]);
+});
