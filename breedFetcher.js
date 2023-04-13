1125556589cc995ab4f7fed59e50b40b06292a11
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-13 19:43:31 -0400

    first entry selectio updated

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,10 @@
 const request = require('request');
 
 request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+  // if (error){
+  //   console.error(error);
+  //   return
+  // }
   console.log('error:', error); 
   console.log('statusCode:', response && response.statusCode); 
   // console.log('body:', body); 
@@ -8,6 +12,6 @@ request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, bo
   const data = JSON.parse(body);
   // console.log(data);
   // console.log(typeof data);
-
-  console.log(data[0]);
+  const firstEntry = data[0];
+  console.log(firstEntry.description);
 });
